58fd688e2ec8ba09ba44fc8f95880d74b5857bed
Vallabh Kansagara <vrkansagara@gmail.com>
2020-01-30 20:57:32 +0530

    fixing image tag issue.

--- modules/Application/config/module.config.php
+++ modules/Application/config/module.config.php
@@ -106,32 +106,7 @@ $navigation = [
                 ],
             ],
         ],
-        [
-            'label' => 'Blog with ORM',
-            'route' => 'blog',
-            'pages' => [
-                [
-                    'label' => 'Detail',
-                    'route' => 'blog',
-                    'action' => 'detail',
-                ],
-                [
-                    'label' => 'Add',
-                    'route' => 'blog/add',
-                    'action' => 'add',
-                ],
-                [
-                    'label' => 'Edit',
-                    'route' => 'blog/add',
-                    'action' => 'edit',
-                ],
-                [
-                    'label' => 'Delete',
-                    'route' => 'blog/add',
-                    'action' => 'delete',
-                ],
-            ],
-        ],
+
     ],
 ];
 return [
